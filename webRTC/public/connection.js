6f8d04fac98fef075a38396fdfefe6f15a683bef
Quent <quentin.fau74@gmail.com>
2017-02-06 14:58:50 +0100

    commit

--- webRTC/public/connection.js
+++ webRTC/public/connection.js
@@ -4,3 +4,6 @@ function Connection(id, sendChannel, receiveChannel) {
     this.receiveChannel = receiveChannel;
     console.log('Nouvel objet Connection créé : send = ' + this.sendChannel.label  + " receive = " + this.receiveChannel.label);
 }
+function Connection(){
+
+}
