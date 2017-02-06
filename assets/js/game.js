5dfd7c566b74dae1d5c9505b13c8e9b57d6c8a8f
Reeker <Reeker@130.190.29.221>
2017-02-06 17:59:05 +0100

    KBE - Ajout du game over quand on sort du rond (qui n'existe pas)

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,14 +1,14 @@
 var player, speed, cursors;
 
-const WORLD_WIDTH = 2000, WORLD_HEIGHT = 2000;
+const WORLD_WIDTH = 400300, WORLD_HEIGHT = 400300;
 const ROTATE_SPEED=200;
 const MAX_PLAYER_SPEED=10,MIN_PLAYER_SPEED=1;
-const INITIAL_SPEED=100, SPEED_MULTIPLICATOR=35;
+const INITIAL_SPEED=634, SPEED_MULTIPLICATOR=35;
+const ROPE_SPEED=10;
 
 var Game = {
 
     preload : function () {
-        //game.load.image('player', './assets/images/balloon.png');
         game.load.spritesheet('player', './assets/images/balloon_animated.png', 300, 150);
         game.load.image('background', './assets/images/background.png');
     },
@@ -25,7 +25,7 @@ var Game = {
         game.physics.enable(player, Phaser.Physics.ARCADE);
         game.camera.follow(player);
         player.body.collideWorldBounds = true;
-        player.animations.add('move', [0, 1, 2, 3, 4, 5, 4, 3, 2, 1], 10, true);
+        player.animations.add('move', [0, 1, 2, 3, 4, 5, 4, 3, 2, 1], ROPE_SPEED, true);
         player.animations.play('move');
     },
 
@@ -48,14 +48,19 @@ var Game = {
 	    else if (game.input.keyboard.isDown(Phaser.Keyboard.UP) && speed <= MAX_PLAYER_SPEED)
 	    {
 	        speed++;
+	        player.animations.currentAnim.speed=ROPE_SPEED*speed;
 	    }
 	    else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN) && speed > MIN_PLAYER_SPEED)
 	    {
 	        speed--;
+	        player.animations.currentAnim.speed=ROPE_SPEED*speed;
 	    }
 	    
 	    game.physics.arcade.velocityFromAngle(player.angle, INITIAL_SPEED+SPEED_MULTIPLICATOR*speed, player.body.velocity);
-
+	    console.log(Math.sqrt(Math.pow(WORLD_WIDTH/2-player.body.x,2)+Math.pow(WORLD_HEIGHT/2-player.body.y,2)));
+	    if (Math.sqrt(Math.pow(WORLD_WIDTH/2-player.body.x,2)+Math.pow(WORLD_HEIGHT/2-player.body.y,2)) >= 8000) {
+	    	game.state.start('Game_Over');
+	    }
     }
 };
 
