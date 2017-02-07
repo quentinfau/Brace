6926263ec8081e5dec35564e773acb95f1f5bdc1
Christophe <christophe@130.190.29.244>
2017-02-07 09:15:08 +0100

    Correction bug export Class PLayerHost And Player

--- webRTC/public/player.js
+++ webRTC/public/player.js
@@ -1,36 +1,41 @@
 
 function Player(name) {
     this.name = name;
-    this.connection = connection;
+    this.connection = null;
     this.radius = 0;
     this.angle = 0;
     this.speed = 0;
     this.coordonneX = 0;
     this.coordonneY = 0;
     console.log('Nouvel objet Player créé : ' + this.name );
+    
+    function setCoordonneX(coordonneX){
+    	this.coordonneX = coordonneX;
+    }
+
+    function setCoordonneY(coordonneY){
+    	this.coordonneY = coordonneY;
+    }
+
+    function setRadius(radius){
+        this.radius = radius;
+    }
+
+    function setSpeed(speed){
+        this.speed = speed;
+    }
+
+    function setAngle(angle){
+        this.angle = angle;
+    }
+
+    function setConnection(connection){
+        this.connection = connection;
+    }
 }
 
-function setCoordonneX(coordonneX){
-	this.coordonneX = coordonneX;
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = Player;
+} else {
+    window.Player = Player;
 }
-
-function setCoordonneY(coordonneY){
-	this.coordonneY = coordonneY;
-}
-
-function setRadius(radius){
-    this.radius = radius;
-}
-
-function setSpeed(speed){
-    this.speed = speed;
-}
-
-function setAngle(angle){
-    this.angle = angle;
-}
-
-function setConnection(connection){
-    this.connection = connection;
-}
-
--- webRTC/public/playerHost.js
+++ webRTC/public/playerHost.js
@@ -1,6 +1,8 @@
 let connections = {};
 let playerList = {};
 
+var method = PlayerHost.prototype;
+
 function PlayerHost(name) {
     this.name = name;
     this.nbJoueur = 1;
@@ -12,35 +14,44 @@ function PlayerHost(name) {
     this.PHSon = null;
     this.timestamp = new Date().getTime();
     console.log('Nouvel objet Player créé : ' + this.name);
+    
+    method.createConnection = function(player) {
+
+    }
+
+    method.setList = function(playerList) {
+        this.playerList = playerList;
+    }
+    
+    function addConnection(connection) {
+        this.connections[connection.id] = connection;
+    }
+
+    function removeConnection(connection) {
+        this.connections.splice(this.connections.indexOf(connection.id), 1);
+    }
+
+    function setPHRightB(PHRightB) {
+        this.PHRightB = PHRightB;
+    }
+
+    function setPHLeftB(PHLeftB) {
+        this.PHLeftB = PHLeftB;
+    }
+
+    function setPHFather(PHFather) {
+        this.PHFather = PHFather;
+    }
+
+    function setPHSon(PHSon) {
+        this.PHSon = PHSon;
+    }
+
 }
 
-function createConnection(player) {
 
-}
-
-function setList(playerList) {
-    this.playerList = playerList;
-}
-function addConnection(connection) {
-    this.connections[connection.id] = connection;
-}
-
-function removeConnection(connection) {
-    this.connections.splice(this.connections.indexOf(connection.id), 1);
-}
-
-function setPHRightB(PHRightB) {
-    this.PHRightB = PHRightB;
-}
-
-function setPHLeftB(PHLeftB) {
-    this.PHLeftB = PHLeftB;
-}
-
-function setPHFather(PHFather) {
-    this.PHFather = PHFather;
-}
-
-function setPHSon(PHSon) {
-    this.PHSon = PHSon;
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = PlayerHost;
+} else {
+    window.PlayerHost = PlayerHost;
 }
