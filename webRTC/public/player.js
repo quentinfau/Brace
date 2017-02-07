8a2b8f4ebc43183ff840f127424c0cf702e7d569
Alex <Alex@Arkh>
2017-02-07 10:32:46 +0100

    a

--- webRTC/public/player.js
+++ webRTC/public/player.js
@@ -1,3 +1,13 @@
+let neighborhood [] ;
+
+var neighbor {
+	name
+	coordonneX
+	coordonneY
+	radius
+	angle
+	speed
+}
 
 function Player(name) {
     this.name = name;
@@ -7,6 +17,7 @@ function Player(name) {
     this.speed = 0;
     this.coordonneX = 0;
     this.coordonneY = 0;
+    this.rank = 1 ;
     console.log('Nouvel objet Player créé : ' + this.name );
     
     function setCoordonneX(coordonneX){
@@ -28,10 +39,56 @@ function Player(name) {
     function setAngle(angle){
         this.angle = angle;
     }
+       
+    function setRank(rank1){
+    	this.rank = rank1;
+    }
 
     function setConnection(connection){
         this.connection = connection;
     }
+    
+    function getCoordonneX(){
+    	return this.CoordonneX;
+    }
+    
+    function getCoordonneY(){
+    	return this.CoordonneY;
+    }
+    
+    function getAngle(){
+    	return this.angle;
+    }
+    
+    function getRadius(){
+    	return this.radius;
+    }
+
+    function getSpeed(){
+    	return this.speed;
+    }
+    
+    function getRank(){
+    	return this.rank;
+    }
+
+    
+    function addNeighbor(name1,coordonneX1,coordonneY1,radius1,angle1,speed1) {
+    	var neighbor1;
+    	neighbor1.name = name1;
+    	neighbor1.coordonneX1 = coordonneX1;
+    	neighbor1.coordonneY1 = coordonneY1;
+    	neighbor1.radius1 = radius1;
+    	neighbor1.angle1 = angle1;
+    	neighbor1.speed1 = speed1;
+    	
+    	neighborhood.push(neighbor1); 	
+    }
+    
+    function removeNeighbor(name) {
+        this.neighborhood.splice(this.neighborhood.indexOf(name), 1);
+    }
+ 
 }
 
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
@@ -39,3 +96,5 @@ if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
 } else {
     window.Player = Player;
 }
+
+
