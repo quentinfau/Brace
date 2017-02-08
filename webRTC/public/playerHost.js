b3cbc76f35e69865dc366b5706e97da0fd0fba90
Alex <Alex@Arkh>
2017-02-08 09:35:42 +0100

    Modifs Player-PlayerHost

--- webRTC/public/playerHost.js
+++ webRTC/public/playerHost.js
@@ -1,8 +1,6 @@
-let dataChannels = [];
-let playerList = {};
-let parent;
-let method = PlayerHost.prototype;
 
+<<<<<<< HEAD
+=======
 function PlayerHost(name) {
     this.name = name;
     this.nbJoueur = 1;
@@ -23,7 +21,46 @@ function PlayerHost(name) {
     this.timestamp = new Date().getTime();
     console.log('Nouvel objet PlayerHost créé : ' + this.name);
 }
+>>>>>>> branch 'master' of https://github.com/quentinfau/Brace.git
 
+<<<<<<< HEAD
+=======
+ function createConnection(player) {
+     return new Promise(function (resolve, reject) {
+         parent = this;
+         pcLocal = new RTCPeerConnection(cfg, con);
+         pcLocal.onicecandidate = function () {
+             if (pcLocal.iceGatheringState == "complete" && !offerSent) {
+                 console.log("player : " + player + " host : " + parent.name);
+                 offerSent = true;
+                 sendNegotiation("offer", pcLocal.localDescription, myHost.name, player.name);
+             }
+         };
+         dc1 = pcLocal.createDataChannel(createID(player.name, myHost.name), {reliable: true});
+         activedc = dc1;
+         dc1.onopen = function () {
+             console.log('Connected');
+             dataChannels.push(dc1);
+             let data = {user: "system", message: "the datachannel " + dc1.label + " has been opened"};
+             writeMsg(data);
+             offerSent = false;
+             resolve("CONNECTED");
+         };
+         dc1.onmessage = function (e) {
+             if (e.data.charCodeAt(0) == 2) {
+                 return
+             }
+             let data = JSON.parse(e.data);
+             writeMsg(data);
+         };
+         pcLocal.createOffer(function (desc) {
+             pcLocal.setLocalDescription(desc, function () {
+             }, function () {
+             });
+             console.log("------ SEND OFFER ------");
+>>>>>>> branch 'master' of https://github.com/quentinfau/Brace.git
+
+<<<<<<< HEAD
  function createConnection(player) {
      return new Promise(function (resolve, reject) {
          parent = this;
@@ -58,6 +95,14 @@ function PlayerHost(name) {
              });
              console.log("------ SEND OFFER ------");
 
+
+let PlayerHost = function (){
+
+	   // console.log('Nouvel objet PlayerHost créé : ' );
+	this.dataChannels = [];
+	this.playerList = {};
+	this.parent;
+=======
          }, function () {
          }, sdpConstraints);
 
@@ -68,6 +113,7 @@ function PlayerHost(name) {
      });
 }
 
+>>>>>>> branch 'master' of https://github.com/quentinfau/Brace.git
 function sendData(data){
     if (data) {
         dataChannels.forEach(function (dataChannel) {
@@ -79,59 +125,120 @@ function sendData(data){
     return false
 }
 
-method.setList = function (playerList) {
-    this.playerList = playerList;
-};
+	this.name = "bob ";
+	this.nbJoueur = 1;
+	this.indicePremierJoueur = 1;
+	this.indiceDernierJoueur = 1;
+	this.PHLeftB = null;
+	this.PHRightB = null;
+	this.PHFather = null;
+	this.PHSon = null;
 
-function addConnection(connection) {
-    this.connections[connection.id] = connection;
-}
+	// Coordonnées zones :
 
-function removeConnection(connection) {
-    this.connections.splice(this.connections.indexOf(connection.id), 1);
-}
+	this.point1 = [1,1];
+	this.point2 = [2,2];
+	this.point3 = [3,3];
+	this.point4 = [4,4];
+		
+	this.timestamp = new Date().getTime();
 
-function setPHRightB(PHRightB) {
-    this.PHRightB = PHRightB;
-}
 
-function setPHLeftB(PHLeftB) {
-    this.PHLeftB = PHLeftB;
-}
+	this.createConnection = function(player) {
+	
+	    parent = this;
+	    pcLocal = new RTCPeerConnection(cfg, con);
+	    pcLocal.onicecandidate = function () {
+	        if (pcLocal.iceGatheringState == "complete" && !offerSent) {
+	            offerSent = true;
+	            sendNegotiation("offer", pcLocal.localDescription, parent.name, player.name);
+	        }
+	    };
+	    dc1 = pcLocal.createDataChannel(createID(player.name, name), {reliable: true});
+	    activedc = dc1;
+	    dc1.onopen = function () {
+	        console.log('Connected');
+	        dataChannels.push(dc1);
+	        let data = {user: "system", message: "the datachannel " + dc1.label + " has been opened"};
+	        writeMsg(data);
+	        offerSent = false;
+	    };
+	    dc1.onmessage = function (e) {
+	        if (e.data.charCodeAt(0) == 2) {
+	            return
+	        }
+	        let data = JSON.parse(e.data);
+	        writeMsg(data);
+	    };
+	    pcLocal.createOffer(function (desc) {
+	        pcLocal.setLocalDescription(desc, function () {
+	        }, function () {
+	        });
+	        console.log("------ SEND OFFER ------");
+	
+	    }, function () {
+	    }, sdpConstraints);
+	
+	    pcLocalList[createID(player, this)] = pcLocal;
+	    if (dc1 != null) {
+	        dcList[createID(player, this)] = dc1;
+	    }
+	};
 
-function setPHFather(PHFather) {
-    this.PHFather = PHFather;
-}
+	this.setList = function (playerList) {
+		playerList = playerList;
+	};
 
-function setPHSon(PHSon) {
-    this.PHSon = PHSon;
-}
+	this.addConnection = function (connection){
+		connections[connection.id] = connection;
+	};
 
-function getPHRightB() {
-    return PHRightB;
-}
-
-function getPHLeftB() {
-	return PHLeftB;
-}
-
-function getPHFather() {
-	return PHFather;
-}
-
-function getPHSon() {
-	return PHSon;
-}
-
-function setZone(point11,point22,point33,point44) {
-    this.point1 = point11;
-    this.point2 = point22;
-    this.point3 = point33;
-    this.point4 = point44;
-}
-
-function getZone() {
-	return null;
+	this.removeConnection = function(connection){
+	    connections.splice(connections.indexOf(connection.id), 1);
+	};
+	
+	this.setPHRightB = function(PHRightB) {
+	    PHRightB = PHRightB;
+	};
+	
+	this.setPHLeftB = function(PHLeftB) {
+	    PHLeftB = PHLeftB;
+	};
+	
+	this.setPHFather = function(PHFather){
+	    PHFather = PHFather;
+	};
+	
+	this.setPHSon = function(PHSon){
+	    PHSon = PHSon;
+	};
+	
+	this.getPHRightB = function(){
+	    return PHRightB;
+	};
+	
+	this.getPHLeftB = function(){
+		return PHLeftB;
+	};
+	
+	this.getPHFather = function(){
+		return PHFather;
+	};
+	
+	this.getPHSon = function(){
+		return PHSon;
+	};
+	
+	this.setZone = function(point11,point22,point33,point44){
+	    point1 = point11;
+	    point2 = point22;
+	    point3 = point33;
+	    point4 = point44;
+	};
+	
+	this.getZone = function() {
+		return null;
+	};
 }
 
 
