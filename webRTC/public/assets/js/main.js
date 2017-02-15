cce1896d2eb1c13eba1385643c37fd8045be2886
Quent <quentin.fau74@gmail.com>
2017-02-15 10:48:46 +0100

    Merge branch 'master' of https://github.com/quentinfau/Brace
    # Conflicts:
    #	webRTC/public/assets/js/game.js

--- webRTC/public/assets/js/main.js
+++ webRTC/public/assets/js/main.js
@@ -1,7 +1,7 @@
 var game;
 
 // Create a new game instance 600px wide and 450px tall:
-game = new Phaser.Game(720, 1080, Phaser.AUTO, '');
+game = new Phaser.Game(1080, 1080, Phaser.AUTO, '');
 
 // First parameter is how our state will be called.
 // Second parameter is an object containing the needed methods for state functionality
