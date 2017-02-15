fe27ef5eae9b6647923dcbf99f5bbcb7f8b1ecdb
fam38 <martinstevens26@gmail.com>
2017-02-15 14:39:19 +0100

    MSS change stun server 3

--- webRTC/public/webRTC.js
+++ webRTC/public/webRTC.js
@@ -8,7 +8,7 @@ let pcLocal;
 let dc1 = null;
 let dc2 = null;
 
-const cfg = {'iceServers': [{'url': "stun:stun2.l.google.com:19302"}]},
+const cfg = {'iceServers': [{'url': "stun:stun3.l.google.com:19302"}]},
     con = {'optional': [{'DtlsSrtpKeyAgreement': true}]};
 
 const sdpConstraints = {
