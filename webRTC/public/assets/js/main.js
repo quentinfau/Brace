b72b778966dbf2ee96a1ef8ebf0a0c127df1321b
Reeker <Reeker@130.190.30.112>
2017-03-17 09:09:59 +0100

    KBE : Nettoyage des sources (code mort, images inutilisées et autres)

--- webRTC/public/assets/js/main.js
+++ webRTC/public/assets/js/main.js
@@ -1,13 +1,9 @@
 var game;
 
-// Create a new game instance 600px wide and 450px tall:
 game = new Phaser.Game(1080, 1080, Phaser.AUTO, 'brace');
 
-// First parameter is how our state will be called.
-// Second parameter is an object containing the needed methods for state functionality
 game.state.add('Menu', Menu);
 
-// Adding the Game state.
 game.state.add('Game', Game);
 
 game.state.add('Game_Over', Game_Over);
